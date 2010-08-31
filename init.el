4861bdcd77a8218d7ca527af2e674153a9ac95b6
Naohiro Sakuma <n.sakuma@everyleaf.com>
2010-08-31 12:44:24 +0900

    init分割 -> prog-init.el(YAML, CSS, ZenCoding

--- init.el
+++ init.el
@@ -97,47 +97,6 @@
 
 
 
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;
-;;;       YAML-mode
-;;
-
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-;; Unlike python-mode, this mode follows the Emacs convention of not
-;; binding the ENTER key to `newline-and-indent'.  To get this
-;; behavior, add the key definition to `yaml-mode-hook':
-
-(add-hook 'yaml-mode-hook
-          '(lambda ()
-             (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;
-;;;       CSS-mode
-;;
-
-(setq css-indent-offset 2)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;
-;;;       Zencoding-mode   --- HTML,CSS
-;;
-;; git://github.com/chrisdone/zencoding.git
-
-(require 'zencoding-mode)
-
-(add-hook 'html-mode-hook 'zencoding-mode)
-(add-hook 'nxhtml-mode-hook 'zencoding-mode)
-(add-hook 'text-mode-hook 'zencoding-mode)
-(add-hook 'sgml-mode-hook 'zencoding-mode)
-
-
 ;;;; Chrome Extention
 ;; Edit with Emacs - Version: 1.6
 ;; Allow user to edit web-page textareas with Emacs (and other editors).
